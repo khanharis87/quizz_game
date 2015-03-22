5fb424c8d568947e32e982724774245ec5e18a72
Haris <haris.khan87@gmail.com>
2015-03-22 13:53:00 +0100

    bootstrapping css

--- angular-seed/app/app.js
+++ angular-seed/app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module("qanda",[])
+angular.module("qanda",['ui.bootstrap'])
 	.controller('mainCtrl',['$scope','$http',function($scope,$http){
 		$scope.testList = [];
 		$scope.questions = [];
